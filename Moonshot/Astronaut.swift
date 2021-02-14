de5cf94f30070d0569fe0c1d24e76fb5574787f2
Channabasavesh Chandrappa Budihal <cbudihal@apple.com>
2021-02-14 14:08:41 +0530

    Initial Commit
    Initial Commit

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Channabasavesh Chandrappa Budihal on 2/12/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
